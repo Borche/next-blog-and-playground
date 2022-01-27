fb6bc802db1002b3d02eb58c9117033c542a5f23
Andreas Börjesson <borjesson.andreas@gmail.com>
2022-01-27 07:02:14 +0100

    Pre-rendering some json blog posts as markdown

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/Layout";
 import utilStyles from "../styles/Utils.module.css";
 import Link from "next/link";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +23,20 @@ export default function Home() {
         <p>My name is Andreas Mouse</p>
         <p>This is a samplte website- bla bla</p>
         <Link href="/some-post">Some post</Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
